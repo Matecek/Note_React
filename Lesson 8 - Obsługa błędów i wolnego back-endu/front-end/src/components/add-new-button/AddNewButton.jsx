93666c24038efd29ef05f8f81fbe43ca4e58f7cb
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2024-08-19 10:41:59 +0200

    Delete Lesson 8 - Obsługa błędów i wolnego back-endu directory

--- Lesson 8 - Obsługa błędów i wolnego back-endu/front-end/src/components/add-new-button/AddNewButton.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./AddNewButton.module.css";
-
-const AddNewButton = ({ children }) => (
-    <button className={styles["add-new-button"]}>{children}</button>
-);
-
-export { AddNewButton };
